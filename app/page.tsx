b521c3638b816b7487c2741370a5934fdce9dc48
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-06-24 10:51:08 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import Image from "next/image";
+import { TextGenerateEffect } from "@/components/ui/TextGenerationEffect";
 
 export default function Home() {
   return (
@@ -8,13 +9,17 @@ export default function Home() {
         <h1 className="text-red-500">Safdar Ali Shah Portfolio Underconstruction!</h1>
         <Hero />
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="h-screen top-0 left-0 w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.04] bg-grid-black/[0.2] relative flex items-center justify-center">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
         <div className="flex justify-center">
           <div className="max-w-[89w] relative my-20 z-10 md:max-w-2xl lg:max-w-[60vw] flex-col items-center justify-center" >
             <div className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-              Dynamic Web Magic with Next.js
+              <h2>Dynamic Web Magic with Next.js</h2>
+              <TextGenerateEffect
+                className="text-center text-[40px] md:text-3xl lg:text-6xl"
+                words={"Transforming concepts into seamless Experiences"}
+              />
             </div>
           </div>
 
